d4c0ba9d713477b5149aecf28729e3836a7233b9
Ioannis Dimitriou <44471356+argusDob@users.noreply.github.com>
2019-05-10 15:55:04 +0200

    Update main.js

--- main.js
+++ main.js
@@ -363,5 +363,39 @@ if (-1 || 0) alert( 'first' ); // F || F = True;
 if (-1 && 0) alert( 'second' ); // T && F = False
 if (null || -1 && 1) alert( 'third' ); // F || F || T ==>T (execute)
 
+//Write the code which asks for a login with prompt.
+//
+//If the visitor enters "Admin", then prompt for a password, if the input is an empty line or Esc – show “Canceled.”, if it’s another string – then show “I don’t know you”.
+//
+//The password is checked as follows:
+//
+//If it equals “TheMaster”, then show “Welcome!”,
+//Another string – show “Wrong password”,
+//For an empty string or cancelled input, show “Canceled.”
+
+function logIn() {
+    let role = prompt("Who is there?")
+
+    if (role == 'Admin') {
+        console.log("Welcome admin")
+        let password = prompt('Please set the password');
+        if (password == 'TheMaster') {
+            console.log('Welcome');
+        } else if (password == null || password == '') {
+            console.log('Please fill in the password');
+        } else {
+            console.log('Wrong password');
+        }
+
+    } else if (role == '' || role == null) {
+        console.log("Canceled")
+    }
+    else{
+        console.log("Sorry i don't know you")
+    }
+}
+logIn();
+
+
 
 

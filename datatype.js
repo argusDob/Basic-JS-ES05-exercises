9e718368be1d2713f363e21715898872f63ee9a4
Ioannis Dimitriou <44471356+argusDob@users.noreply.github.com>
2019-05-13 11:05:26 +0200

    Create datatype.js

---
+++ datatype.js
@@ -0,0 +1,32 @@
+
+//Data types ES05
+//Monday 13/05
+
+
+
+var x = 16 + "Volvo"; //output: 
+console.log(x);
+
+var q =16 +4 "Volvo" ; // output: 20Volvo 
+
+var z = "Volvo" + 16 + 4; // output: Volvo164 since the first operand is a string, all operands are treated as strings.
+
+var x;           // Now x is undefined
+x = 5;           // Now x is a Number
+x = "John";      // Now x is a String
+
+
+var carName1 = "Volvo XC60";   // Using double quotes
+var carName2 = 'Volvo XC60';   // Using single quotes
+
+
+var answer1 = "It's alright"; //Single quotes inside double quotes
+var answer2 = "He is called 'Johnny'"; //Single quotes inside double quotes
+var answer3 = 'He is called "Johnny"'; //Double quotes inside single quotes
+
+
+var x1 = 34.00;     // double
+var x2 = 34;        // integer
+
+
+

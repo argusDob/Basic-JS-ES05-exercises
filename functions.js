620aa3982316151c50ce65841b2a7f7d23ab8612
Ioannis Dimitriou <44471356+argusDob@users.noreply.github.com>
2019-05-14 13:26:14 +0200

    Update functions.js

--- functions.js
+++ functions.js
@@ -215,6 +215,43 @@ if (n < 1) {
   alert( pow(x, n) );
 }
 
+//these argument are called back arguments
+function ask(question, yes, no) {
+  if (confirm(question)) yes()
+  else no();
+}
+
+
+//call back for the yes answer
+function showOk() {
+  console.log( "You agreed." );
+}
+//call back for the no answer
+function showCancel() {
+  console.log( "You canceled the execution." );
+}
+
+// usage: functions showOk, showCancel are passed as arguments to ask
+ask("Do you agree?", showOk, showCancel);
+
+
+//Write the above code using Function Expression
+
+function ask(question, yes, no) {
+  if (confirm(question)) yes()
+  else no();
+}
+
+
+// I created  thwo functions without names.It is impossible to get access of the functions outside of ask();
+ask(
+"Do you agree?"
+function() { console.log('You agreed,')},
+function() {console.log('You cancel the execution')}
+
+)
+
+
 
 
 

034d6d7ebea015718da83f69fa1d923dbc7abbd1
Ioannis Dimitriou <44471356+argusDob@users.noreply.github.com>
2019-05-15 10:20:38 +0200

    Create objects.js

---
+++ objects.js
@@ -0,0 +1,20 @@
+//Wednesday 15/05
+
+var user ={
+     name : "Ioannis",
+     age : 30,
+     isAdmin: true,
+     "likes OSX" : true
+    
+}
+console.log(user);
+console.log(user);
+//console.log(user[age]) //why this doesnot work?
+//console.log(user.age)
+
+
+var key = prompt("what is the user name?", "");
+console.log(user[key]);
+
+
+

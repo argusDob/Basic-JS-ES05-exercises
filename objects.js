4966c62d6cb891d8b80770d0f5aa7a7daeea2536
Ioannis Dimitriou <44471356+argusDob@users.noreply.github.com>
2019-05-15 13:04:23 +0200

    Update objects.js

--- objects.js
+++ objects.js
@@ -117,5 +117,16 @@ var user = {name: "Ioannis"};
 isEmpty(schedule);
 isEmpty(user);
 
+//Write the code to sum all salaries and store in the variable sum. Should be 390 in the example above.
+var salaries = {
+    John: 100,
+    Ann: 50,
+    Ioannis: 1200
+}
+var sum = 0
+for (var key in salaries){
+    sum += salaries[key];
+}
+console.log(sum);
 
 

541207eddedd0462bcacf1f11ee58ae1df1958d7
Ioannis Dimitriou <44471356+argusDob@users.noreply.github.com>
2019-05-16 13:06:04 +0200

    Update arrays.js

--- arrays.js
+++ arrays.js
@@ -88,3 +88,18 @@ function sum(){
 sum();
 
 
+let map = new Map();
+map.set(1, 'str');
+map.set('1', 'num1');
+map.set(true, 'bool1');
+
+
+console.log(map.get(1));
+console.log(map.get('1'));
+console.log(map.get(true));
+console.log(map.size);
+
+
+
+
+

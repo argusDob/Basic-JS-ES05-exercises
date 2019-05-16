16dc355341693b37778071bb328fbc0e131c492d
Ioannis Dimitriou <44471356+argusDob@users.noreply.github.com>
2019-05-16 10:09:14 +0200

    Create arrays.js

---
+++ arrays.js
@@ -0,0 +1,18 @@
+
+//Thursday 16/05
+
+
+
+//*****************************Arrays**********************************
+
+
+//An array can store elements of any type
+
+
+var arr = ['Apple', {name: 'ioannis'}, true, function(){console.log('Hello')}];
+
+for(var i=0; i<arr.length; i++){
+    console.log(arr);
+}
+
+

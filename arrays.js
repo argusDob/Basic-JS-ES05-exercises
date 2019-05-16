31418f5c7637d93dcdb16f1aecd17c0cf8ea4748
Ioannis Dimitriou <44471356+argusDob@users.noreply.github.com>
2019-05-16 10:23:51 +0200

    Update arrays.js

--- arrays.js
+++ arrays.js
@@ -16,3 +16,24 @@ for(var i=0; i<arr.length; i++){
 }
 
 
+ var fruits = ["apple", "orange", "tangerine"];
+console.log(fruits.pop()); //remove the last element of the list
+console.log(fruits);
+
+//push a new element in the array
+var fruits = ["apple", "orange", "tangerine"];
+fruits.push("Cherrys")
+console.log(fruits);
+//remove the first element
+var fruits = ["apple", "orange", "tangerine"];
+console.log(fruits.shift()); //apple
+
+ var fruits = ["apple", "orange", "tangerine"]
+ console.log(fruits.unshift('Apple'))//insert an element in the first position of the array
+console.log(fruits);
+
+
+ var fruits = ["apple", "orange", "tangerine"]
+ console.log(fruits.unshift('Apple'))//insert an element in the first position of the array
+console.log(fruits);
+

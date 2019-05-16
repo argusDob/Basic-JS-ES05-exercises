0e2e94ff5201759a8e0511be4cdfb138ff837a4e
Ioannis Dimitriou <44471356+argusDob@users.noreply.github.com>
2019-05-16 10:52:45 +0200

    Update arrays.js

--- arrays.js
+++ arrays.js
@@ -37,3 +37,20 @@ console.log(fruits);
  console.log(fruits.unshift('Apple'))//insert an element in the first position of the array
 console.log(fruits);
 
+//Let’s try 5 array operations.
+//
+//Create an array styles with items “Jazz” and “Blues”.
+//Append “Rock-n-Roll” to the end.
+//Replace the value in the middle by “Classics”. Your code for finding the middle value should work for any arrays with odd length.
+//Strip off the first value of the array and show it.
+//Prepend Rap and Reggae to the array.
+
+
+var array = ['jazz','blues'];
+array.push('Rock n roll');
+console.log(array);
+console.log(array.length);
+array[Math.floor((array.length - 1) / 2)] = "Classics";
+console.log(array);
+
+
